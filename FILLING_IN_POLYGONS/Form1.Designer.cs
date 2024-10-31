df2ee8bd47d730c5cd61c56811b146c172f84c43
AndreyTHT <andreythtand@gmail.com>
2024-11-01 03:26:40 +0400

    Добавьте файлы проекта.

---
+++ FILLING_IN_POLYGONS/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FILLING_IN_POLYGONS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorSelection = new System.Windows.Forms.ComboBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.fillType = new System.Windows.Forms.ComboBox();
+            this.PaintOver = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(951, 297);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // colorSelection
+            // 
+            this.colorSelection.FormattingEnabled = true;
+            this.colorSelection.Items.AddRange(new object[] {
+            "Черный",
+            "Красный",
+            "Зеленый",
+            "Синий"});
+            this.colorSelection.Location = new System.Drawing.Point(87, 402);
+            this.colorSelection.Name = "colorSelection";
+            this.colorSelection.Size = new System.Drawing.Size(191, 24);
+            this.colorSelection.TabIndex = 2;
+            this.colorSelection.Text = "Цвет линии";
+            this.colorSelection.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(703, 398);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(206, 78);
+            this.clear.TabIndex = 3;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // fillType
+            // 
+            this.fillType.FormattingEnabled = true;
+            this.fillType.Items.AddRange(new object[] {
+            "ориентированного многоугольника",
+            "неориентированного многоугольника"});
+            this.fillType.Location = new System.Drawing.Point(87, 452);
+            this.fillType.Name = "fillType";
+            this.fillType.Size = new System.Drawing.Size(191, 24);
+            this.fillType.TabIndex = 4;
+            this.fillType.Text = "Тип закрашивания";
+            this.fillType.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // PaintOver
+            // 
+            this.PaintOver.Location = new System.Drawing.Point(383, 399);
+            this.PaintOver.Name = "PaintOver";
+            this.PaintOver.Size = new System.Drawing.Size(212, 77);
+            this.PaintOver.TabIndex = 5;
+            this.PaintOver.Text = "Закрасить";
+            this.PaintOver.UseVisualStyleBackColor = true;
+            this.PaintOver.Click += new System.EventHandler(this.PaintOver_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 551);
+            this.Controls.Add(this.PaintOver);
+            this.Controls.Add(this.fillType);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.colorSelection);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox colorSelection;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.ComboBox fillType;
+        private System.Windows.Forms.Button PaintOver;
+    }
+}
+
---
+++ FILLING_IN_POLYGONS/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FILLING_IN_POLYGONS
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen DrawPen = new Pen(Color.Black, 1);
+        List<Point> VertexList = new List<Point>();
+        private bool isCW = false; // Ориентация многоугольника 
+        bool type;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics(); //инициализация графики
+        }
+
+        // Обработчик события выбора цвета
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (colorSelection.SelectedIndex) // выбор цвета
+            {
+                case 0:
+                    DrawPen.Color = Color.Black;
+                    break;
+                case 1:
+                    DrawPen.Color = Color.Red;
+                    break;
+                case 2:
+                    DrawPen.Color = Color.Green;
+                    break;
+                case 3:
+                    DrawPen.Color = Color.Blue;
+                    break;
+            }
+        }
+
+        // Очистка окна
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            VertexList.Clear();
+        }
+
+        // Обработчик события
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int i = VertexList.Count;
+            VertexList.Add(new Point() { X = e.X, Y = e.Y });
+            g.DrawEllipse(DrawPen, e.X - 2, e.Y - 2, 5, 5);
+
+            if (VertexList.Count > 1)
+            {
+                g.DrawLine(DrawPen, VertexList[i - 1], VertexList[i]);
+            }
+
+            if (e.Button == MouseButtons.Right) // Конец ввода
+            {
+                g.DrawLine(DrawPen, VertexList[i], VertexList[0]);
+            }
+
+        }
+
+        // Обработчик события выбора типа закрашивания
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (fillType.SelectedIndex) // выбор режима вывода
+            {
+                case 0:
+                    type = true;
+                    break;
+                case 1:
+                    type = false;
+                    break;
+            }
+        }
+
+        private void PaintOver_Click(object sender, EventArgs e)
+        {
+            if (type)
+            {
+                orientedPainting();
+            }
+            else coloringAlgorithm();
+        }
+
+        private void coloringAlgorithm()
+        {
+
+            if (VertexList.Count > 2)
+            {
+                // Поиск Ymin и Ymax
+                int Ymin = VertexList[0].Y;
+                int Ymax = VertexList[0].Y;
+                foreach (var point in VertexList)
+                {
+                    if (point.Y < Ymin) Ymin = point.Y;
+                    if (point.Y > Ymax) Ymax = point.Y;
+                }
+
+                // Пробегаем по каждой строке от Ymin до Ymax
+                for (int Y = Ymin; Y <= Ymax; Y++)
+                {
+                    List<int> Xb = new List<int>(); // Список пересечений X
+
+                    // Поиск пересечений текущей строки Y со сторонами многоугольника
+                    for (int i = 0; i < VertexList.Count; i++)
+                    {
+                        int k = 0;
+                        if (i < VertexList.Count - 1)
+                        {
+                            k = i + 1;
+                        }
+
+                        Point Pi = VertexList[i];
+                        Point Pk = VertexList[k];
+
+                        // Условие пересечения строки Y со стороной Pi Pk
+                        if ((Pi.Y < Y && Pk.Y >= Y) || (Pi.Y >= Y && Pk.Y < Y))
+                        {
+                            // Вычисляем координату X пересечения
+                            int xIntersect = Pi.X + (Y - Pi.Y) * (Pk.X - Pi.X) / (Pk.Y - Pi.Y);
+                            Xb.Add(xIntersect); // Добавляем в список пересечений
+                        }
+                    }
+
+                    // Сортируем пересечения по X
+                    Xb.Sort();
+
+                    // Закрашиваем сегменты
+                    for (int j = 0; j < Xb.Count; j += 2)
+                    {
+                        if (j + 1 < Xb.Count)
+                        {
+                            g.DrawLine(DrawPen, Xb[j], Y, Xb[j + 1], Y);
+                        }
+                    }
+                }
+            }
+        }
+
+        // алгоритм закрашивания ориентированного многогранника
+        private void orientedPainting()
+        {
+            if (VertexList.Count > 2)
+            {
+                // Шаг 1: Найти Ymin и Ymax
+                int Ymin = VertexList.Min(p => p.Y);
+                int Ymax = VertexList.Max(p => p.Y);
+                int w = pictureBox1.Width;
+                int h = pictureBox1.Height;
+
+                // Шаг 2: Проверить ориентацию CW
+                isCW = CalculatePolygonOrientation();
+
+                if (isCW)
+                {
+                    //Если CW, то строки области вывода от 0 до Ymin и от Ymax до Yemax закрасить целиком
+                    for (int j = 0; j < Ymin; j++)
+                    {
+                        g.DrawLine(DrawPen, 0, j, w, j);
+                    }
+                    for (int j = Ymax; j < h; j++)
+                    {
+                        g.DrawLine(DrawPen, 0, j, w, j);
+                    }
+                }
+                // Шаг 3: Закрасить от Ymin до Ymax
+                for (int Y = Ymin; Y <= Ymax; Y++)
+                {
+                    List<int> Xl = new List<int>(); // Левая граница
+                    List<int> Xr = new List<int>(); // Правая граница
+
+                    // Шаг 4: Найти пересечения с текущей строкой Y
+                    for (int i = 0; i < VertexList.Count; i++)
+                    {
+                        int k = 0;
+                        if (i < VertexList.Count - 1)
+                        {
+                            k = i + 1;
+                        }
+                        Point Pi = VertexList[i];
+                        Point Pk = VertexList[k];
+
+                        // Условие пересечения строки Y со стороной Pi Pk
+                        if ((Pi.Y < Y && Pk.Y >= Y) || (Pi.Y >= Y && Pk.Y < Y))
+                        {
+                            int xIntersect = Pi.X + (Y - Pi.Y) * (Pk.X - Pi.X) / (Pk.Y - Pi.Y);
+
+                            if ((Pk.Y - Pi.Y) > 0)
+                            {
+                                Xr.Add(xIntersect);
+                            }
+                            else
+                            {
+                                Xl.Add(xIntersect);
+                            }
+                        }
+                    }
+
+                    if (isCW)
+                    {
+                        Xl.Add(w);
+                        Xr.Add(0);
+                    }
+
+                    // Шаг 5: Сортировать пересечения
+                    Xl.Sort();
+                    Xr.Sort();
+
+                    for (int j = 0; j < Xl.Count && j < Xr.Count; j++)
+                    {
+                        if (isCW)
+                        {
+                            g.DrawLine(DrawPen, Xr[j], Y, Xl[j], Y);
+                        }
+                        else
+                        {
+                            g.DrawLine(DrawPen, Xl[j], Y, Xr[j], Y);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        // Метод для вычисления ориентации многоугольника
+        private bool CalculatePolygonOrientation()
+        {
+            int sum = 0;
+            for (int i = 0; i < VertexList.Count; i++)
+            {
+                int k = (i + 1) % VertexList.Count;
+                Point Pi = VertexList[i];
+                Point Pk = VertexList[k];
+                sum += (Pk.X - Pi.X) * (Pk.Y + Pi.Y);
+            }
+            return sum < 0;
+        }
+    }
+}
